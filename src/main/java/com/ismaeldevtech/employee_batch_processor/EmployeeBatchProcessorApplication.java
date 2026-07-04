397261a100888482cb93dc7dafa7fcb2a8c56410
Ismaeldevtech <ismael.dev.tech@gmail.com>
2026-07-04 11:58:21 +0200

    feat: initial project setup with Spring Batch dependencies

---
+++ src/main/java/com/ismaeldevtech/employee_batch_processor/EmployeeBatchProcessorApplication.java
@@ -0,0 +1,13 @@
+package com.ismaeldevtech.employee_batch_processor;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmployeeBatchProcessorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EmployeeBatchProcessorApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ismaeldevtech/employee_batch_processor/EmployeeBatchProcessorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ismaeldevtech.employee_batch_processor;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmployeeBatchProcessorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
